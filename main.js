0a228b046bec20625dc7e13f33dab72da0c3625e
Kr0uK <boris.motz@gmail.com>
2017-01-04 11:09:43 +0100

    p

--- main.js
+++ main.js
@@ -1,19 +1,42 @@
-// Protocole
+// On demande le protocole
 var http = require('http');
+// On demande l'URL
+var url = require("url");
+
+// On requiert le module pour manipuler des fichiers
+var fs = require("fs");
+var head, body;
+
+head = fs.readFile('pages/head.html', 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+   head = data.toString();
+});
+
+body = fs.readFile('pages/index.html', 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    body = data.toString();
+});
 
 // Ca crée le serveur
-var server = http.createServer(function (req, res) {
-    
+var server = http.createServer(function (req, res) {  
+    // On récupère l'URL
+    var page = url.parse(req.url).pathname;
+
     // Le code HTTP + le type de données
     res.writeHead(200, {"Content-Type": "text/html"});
-    
     // Ici on écrit du HTML
-    res.write();
-    
-    
+    res.write(head);
+    res.write(body);
+
+
     // On ferme la page
     res.end();
 });
 
+// Le port sur lequel on écoute
 server.listen(80);
 
